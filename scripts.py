0b667bedb10f5facdb8d51f94ab2b1c5640a8f41
Victor Shamakov <sva1.0@mail.ru>
2021-02-04 15:35:17 +0700

    Переписана и оптимизирована функции fix_marks, remove_chastisements, create_commendation. Исправлены стилевые ошибки в соответствие с PEP8

--- scripts.py
+++ scripts.py
@@ -1,44 +1,31 @@
 import random
 
-from datacenter.models import Mark
-from datacenter.models import Schoolkid
-from datacenter.models import Chastisement
-from datacenter.models import Lesson
-from datacenter.models import Commendation
-from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
+from datacenter.models import (
+    Mark,
+    Schoolkid,
+    Chastisement,
+    Lesson,
+    Commendation)
+from django.core.exceptions import MultipleObjectsReturned
 
 
-def fix_marks(schoolkid, desired_evaluations):
-    try:
-        Mark.objects.filter(schoolkid=schoolkid,
-                            points__in=[2, 3]).get()
-    except ObjectDoesNotExist:
-        return 'Поздравляем! У вас нет ни одной плохой оценки!'
-    except MultipleObjectsReturned:
-        Mark.objects.filter(schoolkid=schoolkid,
-                            points__in=[2, 3])
-
-    if desired_evaluations == 4 or desired_evaluations == 5:
-        Mark.objects.filter(
-            schoolkid=schoolkid,
-            points__in=[2, 3]).update(points=desired_evaluations)
-
-        return 'Поздравляем, вы успешно исправили все плохие оценки.\
-                Учитесь хорошо!'
-    else:
+def fix_marks(schoolkid, point):
+    if point not in [4, 5]:
         return 'Введено неверное значение, попробуйте ввести 4 или 5'
+
+    if Mark.objects.filter(schoolkid=schoolkid, points__in=[2, 3]).exists():
+        Mark.objects.filter(schoolkid=schoolkid,
+                            points__in=[2, 3]).update(points=point)
+        return ('Поздравляем, вы успешно исправили все плохие оценки.'
+                'Учитесь хорошо!')
+    else:
+        return 'Поздравляем! У вас нет ни одной плохой оценки!'
 
 
 def remove_chastisements(schoolkid):
-    try:
-        chistisements = Chastisement.objects.filter(schoolkid=schoolkid).get()
-        chistisements.delete()
-    except ObjectDoesNotExist:
-        return 'Замечания не найдены. Вы молодец!'
-    except MultipleObjectsReturned:
-        chistisements = Chastisement.objects.filter(schoolkid=schoolkid)
-        chistisements.delete()
-        return 'Все замечания успешно удалены'
+    chistisements = Chastisement.objects.filter(schoolkid=schoolkid)
+    chistisements.delete()
+    return 'Все замечания успешно удалены'
 
 
 def create_commendation(full_name, subject_title):
@@ -78,23 +65,18 @@ def create_commendation(full_name, subject_title):
     try:
         child = Schoolkid.objects.get(full_name__contains=full_name)
     except Schoolkid.DoesNotExist:
-        return 'Такого ученика нет в базе данных. \
-                Попробуйте ввести запрос по-другому'
+        return ('Такого ученика нет в базе данных.'
+                'Попробуйте ввести запрос по-другому')
     except MultipleObjectsReturned:
         return 'Слишком много совпадений, уточните поиск'
 
     try:
-        lesson = Lesson.objects.get(
-            year_of_study=child.year_of_study,
-            group_letter=child.group_letter,
-            subject__title__contains=subject_title)
-    except Lesson.DoesNotExist:
-        return 'Такого предмета не существует. Попробуйте ввести по-другому'
-    except MultipleObjectsReturned:
         lesson = Lesson.objects.filter(
             year_of_study=child.year_of_study,
             group_letter=child.group_letter,
             subject__title__contains=subject_title).order_by('-date').first()
+    except Lesson.DoesNotExist:
+        return 'Такого предмета не существует. Попробуйте ввести по-другому'
 
     Commendation.objects.create(
         text=random.choice(commendations),
